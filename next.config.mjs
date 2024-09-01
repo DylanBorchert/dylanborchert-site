c65d5d6014f0110368ebffe0ebddcdd66380f2f9
Dylan Borchert <dylanbor01@gmail.com>
2024-08-31 18:15:39 -0600

    fix: config

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+import { withPayload } from "@payloadcms/next/withPayload";
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+	devIndicators: {
+		appIsrStatus: false,
+	},
+};
+
+export default withPayload(nextConfig);
