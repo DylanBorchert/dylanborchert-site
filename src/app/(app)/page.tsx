bf1c9f0d6c758a8108a89a00acf43f2eb2b48d5a
Dylan <dylanbor01@gmail.com>
2024-09-13 18:56:00 -0600

    chore: spell fix and styling

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,9 +1,9 @@
 "use server";
 import Hero from "#/components/custom/Hero";
 import HeroHeader from "#/components/custom/HeroHeader";
-import { Textfit } from "react-textfit";
 import HeroFooter from "#/components/custom/HeroFooter";
 import About from "#/components/custom/About";
+import Resume from "#/components/custom/Resume";
 
 export default async function Home() {
   return (
@@ -16,10 +16,10 @@ export default async function Home() {
       <section className="h-full dark:bg-dot-white/[0.4] bg-dot-black/[0.4] relative">
         <div className="relative z-10">
           <About />
+          <Resume />
         </div>
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       </section>
-
-    </main >
+    </main>
   );
 }
--- src/components/custom/About.tsx
+++ src/components/custom/About.tsx
@@ -9,15 +9,20 @@ import IntersectingFadeIn from "../IntersectingFadeIn";
 export default function About() {
 
     return (
-        <>
-            <Textfit mode='single' forceSingleModeWidth={true} max={10000} min={0} className="w-full font-bold antialiased text-muted-foreground leading-tight bg-clip-text background-clip-paralax">
-                About Me.
-            </Textfit>
-            <IntersectingFadeIn>
-                <p>Summary</p>
-            </IntersectingFadeIn>
-
-        </>
+      <>
+        <Textfit
+          mode="single"
+          forceSingleModeWidth={true}
+          max={10000}
+          min={0}
+          className="w-full font-bold antialiased text-muted-foreground leading-tight bg-clip-text background-clip-parallax"
+        >
+          About Me.
+        </Textfit>
+        <IntersectingFadeIn>
+          <p>Summary</p>
+        </IntersectingFadeIn>
+      </>
     );
 }
 
