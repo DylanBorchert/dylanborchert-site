d74999877ee474a07ada8601c251206a31f39b9a
DylanBorchert <dylanbor01@gmail.com>
2025-03-15 17:59:53 -0600

    fix: mobile now uses non fixed image for iphone

--- src/app/(payload)/hero-image/route.ts
+++ src/app/(payload)/hero-image/route.ts
@@ -7,27 +7,14 @@ import { getPayload } from "payload";
 export const GET = async (req: NextRequest) => {
 	const query = req.nextUrl.searchParams.get("theme");
 	const payload = await getPayload({ config: configPromise });
-	const isIphone = (await headers()).get("user-agent")?.includes("iPhone");
 
 	const data = await payload.findGlobal({ slug: "home" });
-	const imageConfig = isIphone ? data?.["IOS Image"] : data?.["Hero Image"];
+	const imageConfig = data?.["Hero Image"];
 
-	let imageMode: string | undefined;
-	let darkImage: Media | undefined;
-	let lightImage: Media | undefined;
-	let generalImage: Media | undefined;
-
-	if (isIphone && "imageModeIOS" in imageConfig) {
-		imageMode = imageConfig.imageModeIOS;
-		darkImage = imageConfig.darkImageIOS as Media;
-		lightImage = imageConfig.lightImageIOS as Media;
-		generalImage = imageConfig.generalImageIOS as Media;
-	} else if (!isIphone && "imageMode" in imageConfig) {
-		imageMode = imageConfig.imageMode;
-		darkImage = imageConfig.darkImage as Media;
-		lightImage = imageConfig.lightImage as Media;
-		generalImage = imageConfig.generalImage as Media;
-	}
+	const imageMode = imageConfig.imageMode;
+	const darkImage = imageConfig.darkImage as Media;
+	const lightImage = imageConfig.lightImage as Media;
+	const generalImage = imageConfig.generalImage as Media;
 
 	let imageUrl = generalImage?.url as string;
 
